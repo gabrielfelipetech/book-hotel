4e2d7535fd366ea5d98a9ef59a41739a2642e14b
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-17 21:30:00 -0300

    create main views

---
+++ src/views/HomeHotel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="tete">
+    <h1 class="bg-blue-500">Home Hotel</h1>
+    <BInput />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BInput from '@/components/baseComponents/BInput.vue';
+
+const tete = true;
+</script>
---
+++ src/views/HotelReservation.vue
@@ -0,0 +1,9 @@
+<template>
+  <div v-if="tete">
+    <h1 class="bg-blue-500">Home Hotel</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const tete = true;
+</script>
