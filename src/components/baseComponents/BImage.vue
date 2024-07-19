4680f4dec483437fbc98ac488f0f8259771adcf3
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-19 14:55:22 -0300

    create base component image

---
+++ src/components/baseComponents/BImage.vue
@@ -0,0 +1,12 @@
+<template>
+  <img :src="imageProps.src" :alt="imageProps.alt" :class="imageProps.style" />
+</template>
+<script lang="ts" setup>
+type imageProps = {
+  src: string;
+  alt: string;
+  style?: string;
+};
+
+const props = defineProps<{ imageProps: imageProps }>();
+</script>
