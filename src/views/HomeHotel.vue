17e0739b03f4c8ec374d55fd3fdd7f09ecda30e6
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-19 14:57:16 -0300

    adjust home hotel

--- src/views/HomeHotel.vue
+++ src/views/HomeHotel.vue
@@ -1,12 +1,11 @@
 <template>
-  <div v-if="tete">
-    <h1 class="bg-blue-500">Home Hotel</h1>
-    <BInput />
+  <div class="w-full h-full flex flex-col">
+    <HeaderHotel />
+    <HotelList />
   </div>
 </template>
 
 <script setup lang="ts">
-import BInput from '@/components/baseComponents/BInput.vue';
-
-const tete = true;
+import HeaderHotel from '@/components/sharedComponents/HeaderHotel.vue';
+import HotelList from '@/components/sharedComponents/HotelList.vue';
 </script>
