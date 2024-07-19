b23679d4637a2c7c286b3b93937c458da04e2ae3
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-19 20:38:07 -0300

    create card hotel component

---
+++ src/components/sharedComponents/CardHotel.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <BImage
+      :imageProps="{ src: cardHotel.image, alt: cardHotel.name, style: 'h-24' }"
+    />
+    <div class="card-content">
+      <h2>Nome: {{ cardHotel.name }}</h2>
+      <p>Localização: {{ cardHotel.location }}</p>
+      <p>Descrição: {{ cardHotel.descriptionType }}</p>
+      <p>Preço: {{ cardHotel.price }}</p>
+      <p>Nota: {{ cardHotel.rating }}</p>
+      <button>{{ buttonLabel }}</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, defineProps } from 'vue';
+import BImage from '../baseComponents/BImage.vue';
+
+type CardHotelProps = {
+  id: number;
+  name: string;
+  location: string;
+  reserved: boolean;
+  price: number;
+  rating: number;
+  image: string;
+  accommodates: number;
+  type: string;
+  descriptionType: string;
+  rooms: number;
+  availableCheckIn: string;
+  availableCheckOut: string;
+};
+
+const props = defineProps<{ cardHotel: CardHotelProps }>();
+const buttonLabel = computed(() =>
+  props.cardHotel.reserved ? 'Ver detalhes' : 'Reservar',
+);
+</script>
+
+<style scoped></style>
