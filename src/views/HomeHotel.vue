881fe11c50ea68c5206756ba5e46927d4181bc10
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-23 09:21:42 -0300

    create home hotel

--- src/views/HomeHotel.vue
+++ src/views/HomeHotel.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="w-full h-full flex flex-col">
     <HeaderHotel />
-    <HotelList />
+    <HotelList hotels-type="total" :is-comparable="true" />
   </div>
 </template>
 
 <script setup lang="ts">
 import HeaderHotel from '@/components/sharedComponents/HeaderHotel.vue';
 import HotelList from '@/components/sharedComponents/HotelList.vue';
+import useHotelStore from '@/stores/hotel';
+import { onMounted } from 'vue';
+
+const hotelStore = useHotelStore();
+onMounted(() => {
+  hotelStore.getHotels();
+});
 </script>
