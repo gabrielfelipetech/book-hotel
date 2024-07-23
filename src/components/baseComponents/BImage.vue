42346a0f41784f843fcd370e5da1603f0ed6b975
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-23 09:22:12 -0300

    adjust BImage

--- src/components/baseComponents/BImage.vue
+++ src/components/baseComponents/BImage.vue
@@ -2,11 +2,12 @@
   <img :src="imageProps.src" :alt="imageProps.alt" :class="imageProps.style" />
 </template>
 <script lang="ts" setup>
+import { defineProps } from 'vue';
+
 type imageProps = {
   src: string;
   alt: string;
   style?: string;
 };
-
-const props = defineProps<{ imageProps: imageProps }>();
+defineProps<{ imageProps: imageProps }>();
 </script>
