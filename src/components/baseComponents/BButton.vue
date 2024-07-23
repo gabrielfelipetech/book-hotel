6d913823c9421e0eb73238ca393cbfb40461101a
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-23 09:09:13 -0300

    create base component bbutton

--- src/components/baseComponents/BButton.vue
+++ src/components/baseComponents/BButton.vue
@@ -1,3 +1,33 @@
 <template>
-  <button v-bind="$attrs"></button>
+  <button v-bind="$attrs" @click="handleClick" :class="buttonClasses">
+    {{ buttonText }}
+  </button>
 </template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, computed } from 'vue';
+
+const props = defineProps({
+  buttonText: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'primary',
+    validator: (value: string) => ['primary', 'secondary'].includes(value),
+  },
+});
+
+const emit = defineEmits(['click']);
+
+const handleClick = (event: Event) => {
+  emit('click', event);
+};
+
+const buttonClasses = computed(() =>
+  props.color === 'primary'
+    ? 'bg-green-500 text-white'
+    : 'bg-blue-500 text-white',
+);
+</script>
