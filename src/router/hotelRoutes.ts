9a9820027a4716610d02df615a4b60b4d9f76d19
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-23 09:25:57 -0300

    adjust routes

--- src/router/hotelRoutes.ts
+++ src/router/hotelRoutes.ts
@@ -1,15 +1,9 @@
 import { RouteRecordRaw } from 'vue-router';
-import HotelList from '../views/HotelList.vue';
 import HotelReservation from '../views/HotelReservation.vue';
 
 const hotelRoutes: Array<RouteRecordRaw> = [
   {
-    path: '/hotels',
-    name: 'HotelList',
-    component: HotelList,
-  },
-  {
-    path: '/hotels/:id/reserve',
+    path: '/hotels/:id',
     name: 'HotelReservation',
     component: HotelReservation,
     props: true,
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,11 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import homeRoutes from './homeRoutes';
 import hotelRoutes from './hotelRoutes';
 import reservationRoutes from './reservationRoutes';
-import accountRoutes from './accountRoutes';
 
 const routes: Array<RouteRecordRaw> = [
   ...homeRoutes,
   ...hotelRoutes,
   ...reservationRoutes,
-  ...accountRoutes,
 ];
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
