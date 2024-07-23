2aa84599ebaf8e98fb321e0009dd83c8a369511e
Gabriel Felipe <gabrielfelipea2000@icloud.com>
2024-07-23 09:22:53 -0300

    create base component BSelect

---
+++ src/components/baseComponents/BSelect.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+<template>
+  <select v-model="selectedOption" @change="emitSelection">
+    <option disabled value="">{{ disabledOption }}</option>
+    <option v-for="option in options" :key="option.value" :value="option.value">
+      {{ option.name }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, ref } from 'vue';
+
+defineProps({
+  disabledOption: { type: String, default: 'Filtrar' },
+  options: {
+    type: Array as () => { name: string; value: any }[],
+    required: true,
+  },
+});
+
+const emit = defineEmits(['update:selected']);
+
+const selectedOption = ref('');
+const emitSelection = () => {
+  emit('update:selected', selectedOption.value);
+};
+</script>
